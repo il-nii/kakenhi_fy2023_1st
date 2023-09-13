9f58c631daf95f44b6378f86e3358874eadec970
il-nii <126240837+il-nii@users.noreply.github.com>
2023-09-14 03:28:02 +0900

    Update map.js
    Update pin locations

--- _includes/js/map.js
+++ _includes/js/map.js
@@ -46,42 +46,42 @@ window.conference.awaitReady().then(() => {
     const map = window.conference.map.getMap();
 
     if (typeof map !== 'undefined') {
-        let main_station = L.marker([33.73271, 135.38431], {
+        let main_station = L.marker([35.618052279120086, 139.78076073681018], {
             icon: L.divIcon({
                 className: '',
-                html: '<span class="fa fa-train fa-4x"></span><br> Kii-Tanabe station',
+                html: '<span class="fa fa-train fa-4x"></span><br> Telecom center station',
                 iconSize: [120, 56]
             })
         }).addTo(map);
 
-        let venue = L.marker([33.73420, 135.35194], {
+        let venue = L.marker([35.61881009080275, 139.77751173662017], {
             icon: L.divIcon({
                 className: '',
-                html: '<span class="fas fa-users fa-4x"></span><br>  Main venue (Kamenoi Hotel Kii-Tanabe)',
+                html: '<span class="fas fa-users fa-4x"></span><br>  Main venue (AIST Tokyo Waterfront Annex building)',
                 iconSize: [120, 56]
             })
         }).addTo(map);
 
-        let meiyo_mae = L.marker([33.73658, 135.35854], {
+        let meiyo_mae = L.marker([35.62798196395211, 139.77994010835985], {
             icon: L.divIcon({
                 className: '',
-                html: '<span class="fas fa-bus fa-4x"></span><br>  Meiyo mae (bus stop)',
+                html: '<span class="fas fa-train fa-4x"></span><br>  Tokyo Teleport station',
                 iconSize: [120, 56]
             })
         }).addTo(map);
 
-        let tenjin_1 = L.marker([33.72795, 135.35207], {
+        let tenjin_1 = L.marker([35.6282322990255, 139.77190682608966], {
             icon: L.divIcon({
                 className: '',
-                html: '<span class="fas fa-binoculars fa-4x"></span><br>  Tenjin-zaki',
+                html: '<span class="fas fa-binoculars fa-4x"></span><br>  Statue of Liberty',
                 iconSize: [120, 56]
             })
         }).addTo(map);
 
-        let tenjin_2 = L.marker([33.72724, 135.35699], {
+        let tenjin_2 = L.marker([35.63207067204, 139.77752755712808], {
             icon: L.divIcon({
                 className: '',
-                html: '<span class="fas fa-binoculars fa-4x"></span><br> Tenjin-zaki',
+                html: '<span class="fas fa-binoculars fa-4x"></span><br> Odaiba Beach',
                 iconSize: [120, 56]
             })
         }).addTo(map);
